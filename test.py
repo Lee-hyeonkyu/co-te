39eba6b8101880034bc969ff747ebca28924ffe3
Lee-hyeonkyu <leehyeonkyu@naver.com>
2023-01-09 20:31:06 +0900

    .

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
-x = [3, 4, 61, 23, 0, 6, 7, 8]
+import sys
+input = sys.stdin.readline
 
-for i in x:
-    print(i)
-    x.remove(i)
 
-print(x)
+def star(n):
+    if n == 3:
+        return ["***", "* *", "***"]
+
+    S = star(n//3)
+    L = []
+
+    for i in S:
+        L.append(i*3)
+    for i in S:
+        L.append(i + " "*(n//3) + i)
+    for i in S:
+        L.append(i*3)
+    return L
+
+
+n = int(input())
+print("\n".join(star(n)))
