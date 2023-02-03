df1147177bea208d07eaf60341afa0b8e675b286
Lee-hyeonkyu <leehyeonkyu@naver.com>
2023-02-03 14:58:36 +0900

    .

--- sort_.py
+++ sort_.py
@@ -106,3 +106,46 @@ def heap_sort(array):
 
             root = child
     return array
+
+
+# Merge sort (병합정렬)
+# 분할정복, 재귀 사용하는 정렬알고리즘
+# 시간복잡도 O(N)
+
+
+def merge_sort(arr):
+    def sort(low, high):
+        if high - low < 2:
+            return
+        mid = (high + low) // 2
+        sort(low, mid)
+        sort(mid, high)
+        merge(low, mid, high)
+
+    def merge(low, mid, high):
+        tmp = []
+        l, h = low, mid
+        while l < mid and h < high:
+            if arr[l] < arr[h]:
+                tmp.append(arr[l])
+                l += 1
+            else:
+                tmp.append(arr[h])
+                h += 1
+
+        while l < mid:
+            tmp.append(arr[l])
+            l += 1
+        while h < high:
+            tmp.append(arr[h])
+            h += 1
+
+        for i in range(low, high):
+            arr[i] = tmp[i - low]
+
+    return sort(0, len(arr))
+
+
+tt = [3, 6, 4, 1, 5, 7, 9]
+
+print(merge_sort(tt), tt)
